e93bbb0a97e5b3fe963892dc01707b6865f37b22
Hrithic <hrithicraj730@gmail.com>
2024-08-20 21:31:11 +0530

    theme

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import react, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [bgcolor, setBgColor] = useState('aqua');
+  let changeTheme=()=>{
+    (bgcolor==='aqua')? setBgColor('black') : setBgColor('aqua');
+  }
+  return (
+    <div style={{backgroundColor: bgcolor ,transition:'1s'}} className="App">
+      <h4 style={{color:"white"}}>{bgcolor==='aqua'?"Switch to Dark Theme":"Switch to Light Theme"}</h4>
+      <button className='btn' onClick={changeTheme}>
+        <div className='dot' style={{left : (bgcolor==='aqua')? "5px" : "55px", transition:'1s'}}></div>
+      </button>
+    </div>
+  );
+}
+
+export default App;
